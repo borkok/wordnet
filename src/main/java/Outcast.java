bd8fb9791f529f2df79f63a34ea33136116c3d39
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-05-24 11:23:43 +0200

    empty classes

---
+++ src/main/java/Outcast.java
@@ -0,0 +1,11 @@
+public class Outcast {
+    public Outcast(WordNet wordnet) {}         // constructor takes a WordNet object
+
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns) {
+        return null;
+    }
+
+    // see test client below
+    public static void main(String[] args) {}
+}
