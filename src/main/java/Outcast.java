9c744e6cd66fa4acf25a0275badecff477b42743
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-07-03 14:09:20 +0200

    autograder: get rid of 2D array

--- src/main/java/Outcast.java
+++ src/main/java/Outcast.java
@@ -3,7 +3,6 @@ import java.util.stream.Stream;
 
 public class Outcast {
     private final WordNet wordnet;
-    private int[][] distancesByNounPairsCache;
 
     public Outcast(WordNet wordnet) {
         this.wordnet = wordnet;
@@ -12,15 +11,13 @@ public class Outcast {
     // given an array of WordNet nouns, return an outcast
     public String outcast(String[] nouns) {
         NounDistance[] distanceSums = new NounDistance[nouns.length];
-        initializeCache(nouns.length);
 
         for (int fromIndex = 0; fromIndex < nouns.length; fromIndex++) {
             String from = nouns[fromIndex];
             int distanceSum = 0;
 
-            for (int toIndex = 0; toIndex < nouns.length; toIndex++) {
-                String to = nouns[toIndex];
-                int distance = findDistance(from, fromIndex, to, toIndex);
+            for (String to : nouns) {
+                int distance = findDistance(from, to);
                 distanceSum += distance;
             }
 
@@ -33,27 +30,8 @@ public class Outcast {
                      .orElse(null);
     }
 
-    private void initializeCache(int length) {
-        distancesByNounPairsCache = new int[length][length];
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                distancesByNounPairsCache[i][j] = i == j ? 0 : -1;
-            }
-        }
-    }
-
-    private int findDistance(String from, int fromIndex, String to, int toIndex) {
-        int distance = distancesByNounPairsCache[fromIndex][toIndex];
-        if (distance >= 0) return distance;
-
-        return calculateAndCacheDistance(from, fromIndex, to, toIndex);
-    }
-
-    private int calculateAndCacheDistance(String from, int fromIndex, String to, int toIndex) {
-        int distance = wordnet.distance(from, to);
-        distancesByNounPairsCache[fromIndex][toIndex] = distance;
-        distancesByNounPairsCache[toIndex][fromIndex] = distance;
-        return distance;
+    private int findDistance(String from, String to) {
+        return wordnet.distance(from, to);
     }
 
     private static class NounDistance {
